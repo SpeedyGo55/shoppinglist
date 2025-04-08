51de823a559bf5e5bbe2de613f8a13215f763c7a
SpeedyGo55 <83915114+SpeedyGo55@users.noreply.github.com>
2025-04-08 08:45:23 +0200

    setup

---
+++ src/main/java/com/speedygo55/ModItems.java
@@ -0,0 +1,36 @@
+package com.speedygo55;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+
+import java.util.function.Function;
+
+public class ModItems {
+    public static final Item SHOPPING_LIST = register("shoppinglist", Item::new, new Item.Settings());
+    public static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
+        // Create the item key.
+        RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(Shoppinglist.MOD_ID, name));
+
+        // Create the item instance.
+        Item item = itemFactory.apply(settings.registryKey(itemKey));
+
+        // Register the item.
+        Registry.register(Registries.ITEM, itemKey, item);
+
+        return item;
+    }
+
+    public static void initialize() {
+        // Get the event for modifying entries in the ingredients group.
+        // And register an event handler that adds our item to the ingredients group.
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS)
+                .register((itemGroup) -> itemGroup.add(ModItems.SHOPPING_LIST));
+    }
+
+}
