1f7ded725934932341be11bd00831f517c6e15a4
SpeedyGo55 <83915114+SpeedyGo55@users.noreply.github.com>
2025-04-08 09:38:08 +0200

    added actionevent for the Shoppinglist. renamed ModItems.java to ShoppinglistItem.java

--- src/main/java/com/speedygo55/Shoppinglist.java
+++ src/main/java/com/speedygo55/Shoppinglist.java
@@ -18,7 +18,7 @@ public class Shoppinglist implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-		ModItems.initialize();
+		ShoppinglistItem.initialize();
 		LOGGER.info("Shoppinglist initialized!");
 	}
 }
--- src/main/java/com/speedygo55/ShoppinglistItem.java
+++ src/main/java/com/speedygo55/ShoppinglistItem.java
@@ -2,18 +2,29 @@ package com.speedygo55;
 
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.registry.FuelRegistryEvents;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
+import net.minecraft.world.World;
 
 import java.util.function.Function;
 
-public class ModItems {
-    public static final Item SHOPPING_LIST = register("shopping_list", Item::new, new Item.Settings().maxCount(1));
+public class ShoppinglistItem extends Item {
+    @SuppressWarnings("unused")
+    public static final Item SHOPPING_LIST = register("shopping_list", ShoppinglistItem::new, new Item.Settings().maxCount(1));
+
+    public ShoppinglistItem(Settings settings) {
+        super(settings);
+    }
+
     public static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
         // Create the item key.
         RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(Shoppinglist.MOD_ID, name));
@@ -24,20 +35,23 @@ public class ModItems {
         // Register the item.
         Registry.register(Registries.ITEM, itemKey, item);
 
+        // Register the item in the item group.
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register((itemGroup) -> itemGroup.add(item));
+
+        // Register the item as a fuel.
+        FuelRegistryEvents.BUILD.register((builder, context) -> builder.add(item, 5 * 20));
+
         return item;
     }
 
     public static void initialize() {
-        // Add the suspicious substance to the registry of fuels, with a burn time of 30 seconds.
-        // Remember, Minecraft deals with logical based-time using ticks.
-        // 20 ticks = 1 second.
-        FuelRegistryEvents.BUILD.register((builder, context) -> {
-            builder.add(ModItems.SHOPPING_LIST, 5 * 20);
-        });
-        // Get the event for modifying entries in the ingredients group.
-        // And register an event handler that adds our item to the ingredients group.
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS)
-                .register((itemGroup) -> itemGroup.add(ModItems.SHOPPING_LIST));
+    }
+
+    @Override
+    public ActionResult use(World world, PlayerEntity user, Hand hand) {
+        user.sendMessage(Text.of("You used the shopping list!"), true);
+
+        return ActionResult.SUCCESS;
     }
 
 }
